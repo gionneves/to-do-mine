f25e72205b6977b82b3902fb9bdf67b42d90f07a
Giovanni <gionneves@gmail.com>
2021-05-29 17:39:30 -0300

    initial init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="text-center">
+        <button class="btn btn-outline-success m-2" @click="handleCLick('title')">Order by Title</button>
+        <button class="btn btn-outline-success m-2" @click="handleCLick('salary')">Order by Salary</button>
+        <button class="btn btn-outline-success m-2" @click="handleCLick('location')">Order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from "vue";
+import JobList from "./components/JobsList.vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "Farm worker", location: "Lon lon ranch", salary: 30000, id: "1" },
+      { title: "Quarry", location: "Death mountain", salary: 40000, id: "2" },
+      { title: "Flute player", location: "Lost woods", salary: 35000, id: "3" },
+      { title: "Fisherman", location: "Lake Hylia", salary: 2300, id: "4" },
+      { title: "Prision guard", location: "Gerudo valley", salary: 32000, id: "5" },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+
+    const handleCLick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleCLick, order };
+  },
+  methods: {},
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,47 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
+    integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ"
+    crossorigin="anonymous"
+  />
+  <div class="job-list">
+    <div class="container">
+      <ul>
+        <p>Ordered by {{ order }}</p>
+        <li class="container border shadow m-3" v-for="job in jobs" :key="job.id">
+          <h2>{{ job.title }} in {{ job.location }}</h2>
+          <div class="salary text-success">
+            <p><i class="fas fa-money-bill-wave"></i> {{ job.salary }} Ruppes</p>
+          </div>
+          <div class="description">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi voluptates suscipit quidem omnis! Magnam
+              autem laudantium animi, deleniti eos minima facere voluptate labore nostrum quae eligendi exercitationem.
+              Debitis, nostrum necessitatibus!
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
+    },
+  },
+});
+</script>
